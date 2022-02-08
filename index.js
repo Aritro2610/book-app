943dbbab7870c6bdca05d624ce8aca42974409f3
BrandHunt <brandhuntofficial@gmail.com>
2022-02-09 01:05:35 +0530

    make me better

--- index.js
+++ index.js
@@ -47,4 +47,9 @@ app.use('/',indexRouter)
 app.use('/authors',authorRouter)
 app.use('/books',bookRouter)
 
-app.listen(process.env.PORT || 3000)
+// app.listen(process.env.PORT || 3000)
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
