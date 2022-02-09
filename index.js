ba1f7a7848447b4413433d5eb5e7ca2e8e8609a7
BrandHunt <brandhuntofficial@gmail.com>
2022-02-09 10:42:36 +0530

    now commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 //     require('dotenv').config({ path: "config.env" })
 // }
 require('dotenv').config();
-// require('dotenv').config({ path: "./config.env" });
 
 const express = require('express');
 const app = express()
@@ -47,9 +46,7 @@ app.use('/',indexRouter)
 app.use('/authors',authorRouter)
 app.use('/books',bookRouter)
 
-app.listen(process.env.PORT || 3000)
-// var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-// var server_host = process.env.YOUR_HOST || '0.0.0.0';
-// server.listen(server_port, server_host, function() {
-//     console.log('Listening on port %d', server_port);
-// });
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`server ruuning on port ${PORT}`)
+})
