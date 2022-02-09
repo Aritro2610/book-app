d5b92d85bc6c81104a9a154a446eea6768c65632
BrandHunt <brandhuntofficial@gmail.com>
2022-02-09 21:33:01 +0530

    make it now

--- index.js
+++ index.js
@@ -3,23 +3,34 @@
 // }
 require('dotenv').config();
 
-const express = require('express');
+import express, { static } from 'express';
 const app = express()
-const path = require('path')
-const layouts = require('express-ejs-layouts')
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/author')
-const bookRouter = require('./routes/books')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const methodOverride = require('method-override')
+import { join } from 'path';
+import layouts from 'express-ejs-layouts';
+import indexRouter from './routes/index';
+import authorRouter from './routes/author';
+import bookRouter from './routes/books';
+import { urlencoded } from 'body-parser';
+import cors from 'cors';
+import methodOverride from 'method-override';
+
+// const express = require('express');
+// const app = express()
+// const path = require('path')
+// const layouts = require('express-ejs-layouts')
+// const indexRouter = require('./routes/index')
+// const authorRouter = require('./routes/author')
+// const bookRouter = require('./routes/books')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const methodOverride = require('method-override')
 
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views')
 app.set('layout', 'layouts/layout')
 app.use(layouts)
-app.use('/',express.static(path.join(__dirname,'/public')));
-app.use(bodyParser.urlencoded({limit: '10mb' , extended:false}))
+app.use('/',static(join(__dirname,'/public')));
+app.use(urlencoded({limit: '10mb' , extended:false}))
 app.use(cors())
 app.use(methodOverride('_method'))
 
@@ -31,14 +42,15 @@ app.use(methodOverride('_method'))
 //         res.send("Api running")
 //     })
 // }
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI ,{
+import { connect, connection } from 'mongoose';
+// const mongoose = require('mongoose');
+connect(process.env.MONGODB_URI ,{
     useNewUrlParser: true
 })
 
 
 
-const db = mongoose.connection
+const db = connection
 db.on('error',error=> console.error(error));
 db.once('open',()=> console.log('connected to Mongoose'));
 
